07aba0ae571653c5ca99b1a8ed641107c338e3d7
tessein <tessein@gmail.com>
2015-11-04 11:52:59 -0500

    Update update_career_site_in_asignment.sql

--- update_career_site_in_asignment.sql
+++ update_career_site_in_asignment.sql
@@ -1,10 +1,5 @@
-#####################################################################################################
 /* Convert all XXXXXXXX (Client id --xxxxxxxx) careerSite assignments to forwarded career site */
 UPDATE Assignment
-
 INNER JOIN Client     ON Assignment.clientID     = Client.id     AND Client.id = '999999999' AND Client.isMerged = 'true'
 INNER JOIN CareerSite ON Assignment.careerSiteID = CareerSite.id AND CareerSite.forwardCareerSiteID != '0'
-
 SET Assignment.careerSiteID = CareerSite.forwardCareerSiteID;
-
-#####################################################################################################
