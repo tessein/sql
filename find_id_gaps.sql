68509219e988abab012a72d90e8c4acdf835e458
Chuck Tessein <tessein@gmail.com>
2015-11-19 11:25:37 -0500

    Update find_id_gaps.sql

--- find_id_gaps.sql
+++ find_id_gaps.sql
@@ -1,5 +1,6 @@
-select left_side.id + 1 as start
-from the_table_name as left_side
-  left outer join the_table_name as right_side
-    on left_side.id + 1 = right_side.id
-where right_side.id is null;
+/* looking for missing +1 */
+SELECT left_side.id + 1 AS Start_value
+FROM the_table_name AS left_side
+  LEFT OUTER JOIN the_table_name AS right_side
+    ON left_side.id + 1 = right_side.id
+WHERE right_side.id IS NULL;
